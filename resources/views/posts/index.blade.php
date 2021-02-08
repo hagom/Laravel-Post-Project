833fe1a756f66175b73b01efd1afcc3ef47eecb1
Hector Gonzalez <hgonzalezmata@gmail.com>
2021-02-08 00:37:09 -0400

    Añadido las conexiones a base de datos para poder hacer posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="flex justify-center">
     <div class="w-8/12 bg-white p-6 rounded-lg">
-        <form action="{{ route('posts') }}" method="post">
+        <form action="{{ route('posts') }}" method="post" class="mb-4">
             @csrf
             <div class="mb-4">
                 <label for="body" class="sr-body"></label>
@@ -21,6 +21,12 @@
                 <button class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
             </div>
         </form>
+
+        @if ($posts->count())
+        iterate
+        @else
+        <p>There are no posts</p>
+        @endif
     </div>
 </div>
 @endsection
